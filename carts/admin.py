5a55e053234d728f13096a30f72079b19a9415d9
NGUYEN DINH QUANG <quangnd1@aht.local>
2023-07-10 19:57:39 +0700

    add db

--- carts/admin.py
+++ carts/admin.py
@@ -3,13 +3,11 @@ from .models import Cart, CartItem
 
 # Register your models here.
 
-## new 12 - Thêm các dòng sau
 class CartAdmin(admin.ModelAdmin):
     list_display = ('cart_id', 'date_added')
 
 class CartItemAdmin(admin.ModelAdmin):
     list_display = ('product', 'cart', 'quantity', 'is_active')
-## new 12
 
-admin.site.register(Cart)
-admin.site.register(CartItem)
+admin.site.register(Cart, CartAdmin)
+admin.site.register(CartItem, CartItemAdmin)

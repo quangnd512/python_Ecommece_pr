01907af6ee31f1a718241180c89801d2da08421a
quangnd512 <quangnd512@quangnd512-2.local>
2023-07-20 15:52:12 +0700

    Account Activation – Decode User PK & Activate the User | Expire Link

--- accounts/views.py
+++ accounts/views.py
@@ -39,8 +39,8 @@ def register(request):
             send_email = EmailMessage(mail_subject, message, to=[to_email])
             send_email.send()
 
-            messages.success(request, 'Registration successful')
-            return redirect('register')
+            # messages.success(request, 'Registration successful')
+            return redirect('/accounts/login/?command=verification&email='+email)
 
     else:
         form = RegistrationForm()
@@ -68,3 +68,20 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'You are logged out.')
     return redirect('login')
+
+
+def activate(request, uidb64, token):
+    try:
+       uid = urlsafe_base64_decode(uidb64).decode()
+       user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+       user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Congratulations! Your account is activated.')
+        return redirect('login')
+    else:
+        messages.error(requset, 'Invalid activate link')
+        return redirect('register')
--- greatkart/settings.py
+++ greatkart/settings.py
@@ -145,5 +145,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'quangnd512@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'kwzjnmndclzikwqa'
 EMAIL_USE_TLS = True

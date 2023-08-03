579e422c161eab0e2b130ef830a312730adfc6b1
quangnd512 <quangnd512@quangnd512-2.local>
2023-08-03 11:34:05 +0700

    Checkout Page Design

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,10 @@
+from typing import Any
+
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from carts.models import Cart, CartItem
 from store.models import Product, Variation
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 def _cart_id(request):
@@ -123,3 +126,27 @@ def remove_cart_item(request, product_id, cart_item_id):
     cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
     cart_item.delete()
     return redirect('cart')
+
+def checkout(request, total=0, quantity=0, cart_items=None):
+    try:
+        tax = 0
+        grand_total = 0
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity )
+            quantity += cart_item.quantity
+        tax = (total*2)/100
+        grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total,
+    }
+    return render(request, 'store/checkout.html', context)

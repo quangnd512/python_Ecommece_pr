d4318f760009b739bddf1a7b82f0c4c9446b68e5
quangnd512 <quangnd512@quangnd512-2.local>
2023-08-04 11:53:30 +0700

    Assign the User to Cart Item and Modify Cart Counter & Cart View to Handle Logged-in Users

--- carts/views.py
+++ carts/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from carts.models import Cart, CartItem
 from store.models import Product, Variation
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def _cart_id(request):
@@ -84,17 +85,20 @@ class ObjectNotExist:
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
-        cart = Cart.objects.get(cart_id=_cart_id(request))
-        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         tax = 0
         grand_total = 0
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
 
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity )
             quantity += cart_item.quantity
             tax = (total*2)/100
             grand_total = total + tax
-    except ObjectNotExist:
+    except ObjectDoesNotExist:
         pass
 
     context = {
@@ -127,6 +131,7 @@ def remove_cart_item(request, product_id, cart_item_id):
     cart_item.delete()
     return redirect('cart')
 
+@login_required(login_url='login')
 def checkout(request, total=0, quantity=0, cart_items=None):
     try:
         tax = 0
